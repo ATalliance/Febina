5ee76faaf9119c04c942bda9a7b01e917bc383fa
ATalliance <atatalliance2023@gmail.com>
2023-08-21 14:26:17 +0530

    Fixed nested repository issue

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import SignUpPage from './signup';
 
 function App() {
   return (
